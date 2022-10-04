00defcd89809149a34f740f25770e8889604a78b
unknown <benbushman98@gmail.com>
2022-10-04 10:45:15 -0600

    fixed loggedIn info

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -31,7 +31,7 @@ router.get('/dashboard', (req, res) => {
     res.redirect('login');
     return;
   }
-  res.render('dashboard')
+  res.render('dashboard', {loggedIn: req.session.loggedIn})
 });
 
 
