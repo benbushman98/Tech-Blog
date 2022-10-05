93bc27760886dacfc2ff7739877491253329b737
unknown <benbushman98@gmail.com>
2022-10-04 18:30:26 -0600

    style and homepage/dashboard work

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,6 +2,20 @@ const router = require('express').Router();
 const { Post } = require('../../models/');
 const withAuth = require('../../utils/auth');
 
-
+// POST A NEW SURVEY - THIS WORKS ✔️✔️
+router.post('/', withAuth, async (req, res) => {
+    try {
+        console.log("testing");
+      const post = await Post.create({
+        title: req.body.title,
+        content: req.body.content,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
 module.exports = router;
--- public/js/create.js
+++ public/js/create.js
@@ -1,31 +1,23 @@
 //CREATE A SURVEY - THIS WORKS ✔️✔️
 
-const createBtn = document.querySelector('#createBtn');
-const surveyForm = document.querySelector('#surveyForm');
+const createPost = document.querySelector('#createPost');
+const postForm = document.querySelector('#postForm');
 
-function showEditForm() {
-    surveyForm.classList.remove('hide');
-    createBtn.classList.add('hide');
+function showPostCreation() {
+  postForm.classList.remove('hide');
+  createPost.classList.add('hide');
 };
 
-async function newSurvey(event) {
+async function newPost(event) {
     event.preventDefault();
   
-    const title = document.querySelector('#surveyTitle').value;
-    const question = document.querySelector('#surveyQuestion').value;
-    const option1 = document.querySelector('#option1').value;
-    const option2 = document.querySelector('#option2').value;
-    const option3 = document.querySelector('#option3').value;
-    const option4 = document.querySelector('#option4').value;
-    const response = await fetch('/api/survey', {
+    const title = document.querySelector('#postTitle').value;
+    const content = document.querySelector('#postContent').value;
+    const response = await fetch('/api/posts', {
       method: 'POST',
       body: JSON.stringify({
         title: title,
-        question: question,
-        option1: option1,
-        option2: option2,
-        option3: option3,
-        option4: option4
+        content: content,
       }),
       headers: {
         'Content-Type': 'application/json'
@@ -33,13 +25,13 @@ async function newSurvey(event) {
     });
   
     if (response.ok) {
-      createBtn.classList.remove('hide');
-      surveyForm.classList.add('hide');
+      createPost.classList.remove('hide');
+      postForm.classList.add('hide');
       document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
   }
   
-document.querySelector('.newSurveyForm').addEventListener('submit', newSurvey);
-createBtn.addEventListener('click', showEditForm)
+document.querySelector('.newPostForm').addEventListener('submit', newPost);
+createPost.addEventListener('click', showPostCreation)
